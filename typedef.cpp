438256f19329a0025daed6ae0fed2d5fde053555
Jayachandra Nimagadda <96283400+jayachandra01@users.noreply.github.com>
2025-01-21 11:18:22 +0530

    Add files via upload

---
+++ typedef.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+typedef std::string text_t;
+
+int main() {
+    text_t firstName = "Jayachandra";
+    text_t lastName = "Nimagadda";
+    std::cout << firstName + " " + lastName << std::endl; // Concatenate with a space in between
+    return 0;
+}
